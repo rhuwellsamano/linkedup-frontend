b862f37b70bb253bb05f4535ef776a63c79337f1
rhuwellsamano <rhuwell.samano@gmail.com>
2019-04-05 17:27:15 -0400

    aye

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Cable from 'actioncable'
 
 class App extends Component {
+
+  state = {
+    currentChatMessage: '',
+    chatLogs: []
+  }
+
+  componentWillMount() {
+    this.createSocket();
+  }
+
+  updateCurrentChatMessage(event) {
+    this.setState({
+      currentChatMessage: event.target.value
+    });
+  }
+
+  handleSendEvent(event) {
+    event.preventDefault();
+    this.chats.create(this.state.currentChatMessage);
+    this.setState({
+      currentChatMessage: ''
+    });
+  }
+
+  renderChatLog() {
+    return this.state.chatLogs.map((el) => {
+      return (
+        <li key={`chat_${el.id}`}>
+          <span className='chat-message'>{ el.content }</span>
+          <span className='chat-created-at'> { el.created_at }</span>
+        </li>
+      );
+    });
+  }
+
+  handleChatInputKeyPress(event) {
+    if(event.key === 'Enter') {
+      this.handleSendEvent(event);
+    }
+  }
+
+  createSocket() {
+    // let cable = Cable.createConsumer('wss://serene-hollows-71418.herokuapp.com/');
+    // let cable = Cable.createConsumer('wss://274bc527.ngrok.io/cable');
+    // let cable = Cable.createConsumer('ws://10.39.109.8:3001/cable');
+    let cable = Cable.createConsumer('ws://10.39.109.8:3001/cable');
+    this.chats = cable.subscriptions.create({
+      channel: 'ChatChannel'
+    }, {
+      connected: () => {},
+      received: (data) => {
+        let chatLogs = this.state.chatLogs;
+        chatLogs.push(data);
+        this.setState({ chatLogs: chatLogs });
+      },
+      create: function(chatContent) {
+        this.perform('create', {
+          content: chatContent
+        });
+      }
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App'>
+        <div className='stage'>
+          <h1><span role="img" aria-label="Fire">🔥</span> LinkedUp Chatroom <span role="img" aria-label="Fire">🔥</span></h1>
+          <div className='chat-logs'>
+            { this.renderChatLog() }
+          </div>
+          <input
+            onKeyPress={ (e) => this.handleChatInputKeyPress(e) }
+            onChange={ (e) => this.updateCurrentChatMessage(e) }
+            value={ this.state.currentChatMessage }
+            type='text'
+            placeholder='Enter your message...'
+            className='chat-input'/>
+          <button
+            onClick={ (e) => this.handleSendEvent(e) }
+            className='send'>
+            Send
+          </button>
+        </div>
       </div>
     );
   }
